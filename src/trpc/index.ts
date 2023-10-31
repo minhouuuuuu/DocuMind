18490f905fd00c5d9d51805b75e27151fa4e599c
Minh NGUYEN <minhstrasbourg@gmail.com>
2023-10-31 13:45:29 +0100

    dashboard add delete animation

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -40,16 +40,27 @@ export const appRouter = router({
         },
       })
     }),
-    // deleteFile: privateProcedure.input(
-    //   z.object({ id: z.string() })
-    // ).mutation(async ({ctx}) => {
-    //   const {userId} = ctx
+    deleteFile: privateProcedure.input(
+      z.object({ id: z.string() })
+    ).mutation(async ({ ctx, input }) => {
+      const {userId} = ctx
 
-    //   const file = await = await db.file.findFirst({
-    //     where: {
-    //       id:id
-    //     }
-    // })
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        }
+      })
+
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        }
+      })
+      return file
+    }),
 });
 
 export type AppRouter = typeof appRouter;

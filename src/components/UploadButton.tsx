c31fa7622df39bb5608796f2fb0cf82eb17377a8
Minh NGUYEN <minhstrasbourg@gmail.com>
2023-11-02 10:39:01 +0100

    perfecting upload process

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -5,7 +5,7 @@ import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
 import { Button } from "./ui/button"
 
 import Dropzone from "react-dropzone"
-import { Cloud, File } from "lucide-react"
+import { Cloud, File, Loader2 } from "lucide-react"
 import { Progress } from "./ui/progress"
 import { set } from "date-fns"
 import { useUploadThing } from "@/lib/uploadthing"
@@ -16,17 +16,19 @@ import { useRouter } from "next/navigation"
 const UploadDropzone = () => {
     const router = useRouter()
 
-    const [isUploading, setIsUploading] = useState<boolean>(true)
+    const [isUploading, setIsUploading] = useState<boolean>(false)
     const [uploadProgress, setUploadProgress] = useState<number>(0)
     const {toast} = useToast()
 
-    const { mutate: startPolling } = trpc.getFile.useMutation({
-        onSuccess: (file) => {
-            router.push(`/dasboard/${file.id}`)
-        },
-        retry: true,
-        retryDelay: 500,
-    })
+    const { mutate: startPolling } = trpc.getFile.useMutation(
+        {
+          onSuccess: (file) => {
+            router.push(`/dashboard/${file.id}`)
+          },
+          retry: true,
+          retryDelay: 500,
+        }
+      )
 
     const { startUpload } = useUploadThing("pdfUploader")
 
@@ -39,9 +41,9 @@ const UploadDropzone = () => {
                     clearInterval(interval)
                     return prevProgress
                 }
-                return prevProgress 
+                return prevProgress + 1
             })
-        }, 500)
+        }, 100)
 
         return interval
     }
@@ -74,10 +76,10 @@ const UploadDropzone = () => {
                 })
             }
 
-
-
             clearInterval(progressInterval)
             setUploadProgress(100)
+
+            startPolling({ key })
         }}>
             {({ getRootProps, getInputProps, acceptedFiles }) => (
                 <div {...getRootProps()} className="border h-64 m-4 border-dashed border-gray-300 rounded-lg">
@@ -105,11 +107,26 @@ const UploadDropzone = () => {
                             ) : null}
 
                             {isUploading ? (
-                                <div className="w-full mt-4 max-w-xs mx-auto">
-                                    <Progress value={uploadProgress} className="h-1 w-full bg-zinc-200"/>
+                                <div className='w-full mt-4 max-w-xs mx-auto'>
+                                <Progress
+                                    indicatorColor={
+                                    uploadProgress === 100
+                                        ? 'bg-green-500'
+                                        : ''
+                                    }
+                                    value={uploadProgress}
+                                    className='h-1 w-full bg-zinc-200'
+                                />
+                                {uploadProgress === 100 ? (
+                                    <div className='flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2'>
+                                    <Loader2 className='h-3 w-3 animate-spin' />
+                                    Redirecting...
+                                    </div>
+                                ) : null}
                                 </div>
                             ) : null}
 
+                            <input {...getInputProps} type="file" id="dropzone-file" className="hidden" />
                         </label>
                     </div>
                 </div>

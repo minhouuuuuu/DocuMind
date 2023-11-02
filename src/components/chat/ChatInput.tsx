ab4ae2a5855ee1e8ab74088328e683f446241f61
Minh NGUYEN <minhstrasbourg@gmail.com>
2023-11-02 15:36:44 +0100

    instant loading states

---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,11 @@
+interface ChatInputProps{
+    isDisabled: boolean
+}
+
+const ChatInput = ({isDisabled}: ChatInputProps) => {
+  return (
+    <div>ChatInput</div>
+  )
+}
+
+export default ChatInput

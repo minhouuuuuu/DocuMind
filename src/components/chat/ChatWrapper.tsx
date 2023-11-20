18cde4cbe969e4dedd994447908e9d730c248da4
Minh NGUYEN <minhstrasbourg@gmail.com>
2023-11-20 13:28:56 +0100

    adding open ai for chatMessage

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -6,6 +6,7 @@ import Messages from "./Messages"
 import { ChevronLeft, Loader2, XCircle } from "lucide-react"
 import Link from "next/link"
 import { buttonVariants } from "../ui/button"
+import { ChatContextProvider } from "./ChatContext"
 
 interface ChatWrapperProps {
   fileId: string
@@ -78,13 +79,16 @@ const ChatWrapper = ({fileId}: ChatWrapperProps) => {
       )
 
   return (
-    <div className="relative min-h-full bg-zinc-50 flex flex-col divide-y divide-zinc-200 justify-between gap-2">
-      <div className="flex-1 justify-between flex flex-col mb-28">
-        <Messages />
+    <ChatContextProvider fileId={fileId}>
+      <div className="relative min-h-full bg-zinc-50 flex flex-col divide-y divide-zinc-200 justify-between gap-2">
+        <div className="flex-1 justify-between flex flex-col mb-28">
+          <Messages />
+        </div>
+      
+        <ChatInput isDisabled/>
       </div>
-    
-		  <ChatInput isDisabled/>
-    </div>
+    </ChatContextProvider>
+
   )
 }
 
